584f355ae90434c42e6187d35f60fbe9e55d82bd
Tino Breddin <tino@hoprnet.org>
2024-06-03 16:00:15 +0200

    initial setup

---
+++ shell.nix
@@ -0,0 +1,30 @@
+{ pkgs ? import <nixpkgs> { }, ... }:
+let
+  linuxPkgs = with pkgs; lib.optional stdenv.isLinux (
+    inotifyTools
+  );
+  macosPkgs = with pkgs; lib.optional stdenv.isDarwin (
+    with darwin.apple_sdk.frameworks; [
+      # macOS file watcher support
+      CoreFoundation
+      CoreServices
+    ]
+  );
+in
+with pkgs;
+mkShell {
+  buildInputs = [
+    ## base
+    envsubst
+
+    # build utils
+    nodejs_20
+
+    # custom pkg groups
+    macosPkgs
+    linuxPkgs
+  ];
+  shellHook = ''
+    npm install
+  '';
+}
